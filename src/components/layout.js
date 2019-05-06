42816eb8dc9c459a268a411b62bb1861c9af82cc
James Brunner <jbrunner@infiniteelectronics.com>
2019-05-06 14:55:20 -0600

    Change template boilerplate to Brunner Technical Services copy.  Fix whitespace (use tabs)>

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,47 +10,47 @@ const Layout = ({ children, location }) => {
   let content;
 
   if (location && location.pathname === '/') {
-    content = (
-      <div>
-        {children}
-      </div>
-    )
+	content = (
+	  <div>
+		{children}
+	  </div>
+	)
   } else {
-    content = (
-      <div id="wrapper" className="page">
-        <div>
-          {children}
-        </div>
-      </div>
-    )
+	content = (
+	  <div id="wrapper" className="page">
+		<div>
+		  {children}
+		</div>
+	  </div>
+	)
   }
 
   return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
-            }
-          }
-        }
-      `}
-      render={data => (
-        <>
-          <Helmet
-            title={data.site.siteMetadata.title}
-            meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
-            ]}
-          >
-            <html lang="en" />
-          </Helmet>
-          {content}
-        </>
-      )}
-    />
+	<StaticQuery
+	  query={graphql`
+		query SiteTitleQuery {
+		  site {
+			siteMetadata {
+			  title
+			}
+		  }
+		}
+	  `}
+	  render={data => (
+		<>
+		  <Helmet
+			title={data.site.siteMetadata.title}
+			meta={[
+			  { name: 'description', content: 'Brunner Technical Services capabilities portfolio.' },
+			  { name: 'keywords', content: 'software, consulting, technical, design, instrumentation, iot, iiot' },
+			]}
+		  >
+			<html lang="en" />
+		  </Helmet>
+		  {content}
+		</>
+	  )}
+	/>
   )
 }
 

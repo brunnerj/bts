31abf18f3af6b5e60a2ccd03def5a0d89bbed47d
James Brunner <jbrunner@infiniteelectronics.com>
2019-05-06 12:11:40 -0600

    Updates imagery and gatsby configuration to use BTS specifics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
-    },
-    'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
-  ],
+	siteMetadata: {
+		title: "Brunner Technical Services LLC",
+		author: "James Brunner",
+		description: "A small sampling of our capabilities."
+	},
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: 'brunner-technical-services',
+				short_name: 'bts',
+				start_url: '/',
+				background_color: '#663399',
+				theme_color: '#663399',
+				display: 'minimal-ui',
+				icon: 'src/images/icon.png', // This path is relative to the root of the site.
+			},
+		},
+		'gatsby-plugin-sass',
+		'gatsby-plugin-offline'
+	],
 }

4876d8b51d6706199d487e6e133daf00228a0db4
James Brunner <james@brunnertechnicalservices.com>
2019-05-09 20:53:56 -0600

    Moves the Logo and wrapper styles into inline css and removes the scss class.
    Makes the header nav links buttons instead of anchors.
    
    Removes the framework-agnostic 'emotion' module.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,39 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
+
+import Logo from './Logo';
+
 
 const Header = (props) => (
-	<header id="header" style={props.timeout ? {display: 'none'} : {}}>
-		<div className="logo"></div>
+	<header id='header' style={props.timeout ? {display: 'none'} : {}}>
+		
+		<div css={css`
+				width: 5.5rem;
+				height: 5.5rem;
+				line-height: 5.5rem;
+				border: solid 1px white;
+				border-radius: 100%;
+			`}>
+			<Logo color='white' css={css`
+				margin: 0.75rem;
+			`}/>
+		</div>
+
 		<div className="content">
 			<div className="inner">
 				<h1>Brunner</h1>
 				<h2>Technical Services LLC</h2>
-				<p>Building modern, web-enabled interfaces for all your tools and infrastructure.</p>
+				<p>Expert software design for your IIoT infrastructure, equipment automation and analytics.</p>
 			</div>
 		</div>
+
 		<nav>
 			<ul>
-				<li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-				<li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-				<li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-				<li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+				<li><button onClick={() => {props.onOpenArticle('intro')}}>Intro</button></li>
+				<li><button onClick={() => {props.onOpenArticle('work')}}>Work</button></li>
+				<li><button onClick={() => {props.onOpenArticle('about')}}>About</button></li>
+				<li><button onClick={() => {props.onOpenArticle('contact')}}>Contact</button></li>
 			</ul>
 		</nav>
 	</header>

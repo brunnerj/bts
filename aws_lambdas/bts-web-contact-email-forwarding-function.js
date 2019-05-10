b47177b0c0ac2bb44d8b04646f4e792561b2589a
James Brunner <james@brunnertechnicalservices.com>
2019-05-09 20:56:29 -0600

    Track all of the BTS website related AWS lambda functions.

---
+++ aws_lambdas/bts-web-contact-email-forwarding-function.js
@@ -0,0 +1,31 @@
+'use strict';
+const AWS = require('aws-sdk');
+const ses = new AWS.SES();
+ 
+const COMPANY_ADDRESS = 'info@brunnertechnicalservices.com';
+const SENDER_ADDRESS = 'noreply@brunnertechnicalservices.com';
+
+exports.handler = (event, context) => {
+	
+	console.log('Received event: ', event);
+
+	ses.sendEmail({
+		Destination: {
+			ToAddresses: [ COMPANY_ADDRESS ]
+		},
+		ReplyToAddresses: [ event.email ],
+		Message: {
+			Body: {
+				Text: {
+					Data: 'name: ' + event.name + '\nemail: ' + event.email + '\nmessage: ' + event.message,
+					Charset: 'UTF-8'
+				}
+			},
+			Subject: {
+				Data: 'Website Referral from: ' + event.name,
+				Charset: 'UTF-8'
+			}
+		},
+		Source: SENDER_ADDRESS
+	}, (err) => { context.done(err, null) });
+};
---
+++ aws_lambdas/bts-web-preview-authorization-function.js
@@ -0,0 +1,36 @@
+'use strict';
+exports.handler = (event, context, callback) => {
+
+	// Get request and request headers
+	const request = event.Records[0].cf.request;
+	const headers = request.headers;
+	
+	const users = {
+		"admin": "G00se Washed",
+	};
+
+	// Require Basic authentication
+	if (typeof headers.authorization != 'undefined') {
+		const authString = headers.authorization[0].value;
+		const base64 = authString.substring(6);
+		const combo = Buffer.from(base64, 'base64').toString('utf8').split(':');
+		const username = combo[0];
+		const password = combo[1];
+		if(users[username] === password) {
+			// Continue request processing if authentication passed
+			callback(null, request);
+		}
+	}
+	
+	// Otherwise, reject the request
+	const body = 'Unauthorized';
+	const response = {
+		status: '401',
+		statusDescription: 'Unauthorized',
+		body: body,
+		headers: {
+			'www-authenticate': [{key: 'WWW-Authenticate', value:'Basic'}]
+		},
+	};
+	callback(null, response);
+};

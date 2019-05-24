e7d4977effa96105c7949e5ce1bfaede5f698e60
James Brunner <james@brunnertechnicalservices.com>
2019-05-24 11:19:34 -0600

    prettier the aws lambda library

--- aws_lambdas/bts-web-preview-authorization-function.js
+++ aws_lambdas/bts-web-preview-authorization-function.js
@@ -1,27 +1,28 @@
 'use strict';
 exports.handler = (event, context, callback) => {
-
 	// Get request and request headers
 	const request = event.Records[0].cf.request;
 	const headers = request.headers;
-	
+
 	const users = {
-		"admin": "G00se Washed",
+		admin: 'G00se Washed',
 	};
 
 	// Require Basic authentication
 	if (typeof headers.authorization != 'undefined') {
 		const authString = headers.authorization[0].value;
 		const base64 = authString.substring(6);
-		const combo = Buffer.from(base64, 'base64').toString('utf8').split(':');
+		const combo = Buffer.from(base64, 'base64')
+			.toString('utf8')
+			.split(':');
 		const username = combo[0];
 		const password = combo[1];
-		if(users[username] === password) {
+		if (users[username] === password) {
 			// Continue request processing if authentication passed
 			callback(null, request);
 		}
 	}
-	
+
 	// Otherwise, reject the request
 	const body = 'Unauthorized';
 	const response = {
@@ -29,7 +30,7 @@ exports.handler = (event, context, callback) => {
 		statusDescription: 'Unauthorized',
 		body: body,
 		headers: {
-			'www-authenticate': [{key: 'WWW-Authenticate', value:'Basic'}]
+			'www-authenticate': [{ key: 'WWW-Authenticate', value: 'Basic' }],
 		},
 	};
 	callback(null, response);
